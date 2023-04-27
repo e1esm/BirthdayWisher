99ffe860aeb33015f8eeb883756eae6d6dc7a89e
elesm <elesmdev1102@gmail.com>
2023-04-27 19:57:26 +0300

    Soon method's improvements

--- bot/internal/router/soon.go
+++ bot/internal/router/soon.go
@@ -1,10 +1,25 @@
 package router
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strings"
+)
 
 // TODO list all the members that're going to have a birthday this month
 func (r *BirthdayRouter) soon(message tgbotapi.Message) {
 	response, _ := r.ConnectorService.GetSoonBirthdays(message.Chat.ID)
-	msg := tgbotapi.NewMessage(message.Chat.ID, response.String())
+
+	sb := strings.Builder{}
+	sb.WriteString("=====\n*Предстоящие дни рождения этого месяца*\n")
+	for _, birthday := range response.SoonBirthdays {
+		sb.WriteString(birthday.Username)
+		sb.WriteString(" : ")
+		sb.WriteString(birthday.BirthdayDate)
+		sb.WriteString("\n\n")
+	}
+	sb.WriteString("=====\n")
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, sb.String())
+	msg.ParseMode = "markdown"
 	r.bot.Send(msg)
 }

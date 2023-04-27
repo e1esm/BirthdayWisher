67fe60f5af45012f9d0ee4de5758d93db7806e6a
elesm <elesmdev1102@gmail.com>
2023-04-27 20:12:31 +0300

    Fixed enhanced bot's message

--- bot/internal/router/soon.go
+++ bot/internal/router/soon.go
@@ -2,19 +2,25 @@ package router
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strconv"
 	"strings"
+	"time"
 )
 
-// TODO list all the members that're going to have a birthday this month
 func (r *BirthdayRouter) soon(message tgbotapi.Message) {
 	response, _ := r.ConnectorService.GetSoonBirthdays(message.Chat.ID)
 
 	sb := strings.Builder{}
-	sb.WriteString("=====\n*Предстоящие дни рождения этого месяца*\n")
-	for _, birthday := range response.SoonBirthdays {
+	sb.WriteString("*Предстоящие дни рождения этого месяца*\n=====\n")
+	for i, birthday := range response.SoonBirthdays {
 		sb.WriteString(birthday.Username)
 		sb.WriteString(" : ")
-		sb.WriteString(birthday.BirthdayDate)
+		birthdayDate := strconv.Itoa(time.Now().Year()) + birthday.BirthdayDate[4:10]
+		sb.WriteString(birthdayDate)
+		if i == len(response.SoonBirthdays)-1 {
+			sb.WriteString("\n")
+			break
+		}
 		sb.WriteString("\n\n")
 	}
 	sb.WriteString("=====\n")

135734f94a2bc65aa2c871a4488eded7eaa37788
elesm <elesmdev1102@gmail.com>
2023-04-30 22:09:32 +0300

    Added prometheus template, integrated it into congratulationServer service

--- congratulationsGenerator/cmd/main.go
+++ congratulationsGenerator/cmd/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"congratulationsGenerator/internal/service"
 	"congratulationsGenerator/utils"
+	"context"
 	"github.com/e1esm/protobuf/bridge_to_API/gen_proto"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/joho/godotenv"
+	"github.com/oklog/run"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sashabaranov/go-openai"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"syscall"
 )
 
 func main() {
@@ -19,23 +25,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error while reading from env file")
 	}
-
 	token := os.Getenv("AI_TOKEN")
 	port := os.Getenv("GRPC_PORT")
 	name := os.Getenv("AI_CALLER_CONTAINER_NAME")
 	client := openai.NewClient(token)
-	server, err := net.Listen("tcp", name+port)
-	defer server.Close()
 	if err != nil {
 		utils.Logger.Fatal("Address can't be listened", zap.String("error", err.Error()))
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor))
 	aiService := service.NewOpenAIService(client)
 	gen_proto.RegisterCongratulationServiceServer(grpcServer, aiService)
+	grpc_prometheus.Register(grpcServer)
 
-	if err := grpcServer.Serve(server); err != nil {
-		utils.Logger.Fatal("Server can't be started", zap.String("error", err.Error()))
+	group := run.Group{}
+
+	group.Add(func() error {
+		server, err := net.Listen("tcp", name+port)
+		if err != nil {
+			return err
+		}
+		return grpcServer.Serve(server)
+	}, func(err error) {
+		grpcServer.GracefulStop()
+	})
+	httpServer := &http.Server{Addr: name + ":8099"}
+	group.Add(func() error {
+		m := http.NewServeMux()
+		m.Handle("/metrics", promhttp.Handler())
+		httpServer.Handler = m
+		return httpServer.ListenAndServe()
+	}, func(err error) {
+		if err := httpServer.Close(); err != nil {
+			utils.Logger.Fatal("Error while stopping the server", zap.String("msg", "Failed to stop the web server"), zap.String("error", err.Error()))
+		}
+	})
+
+	group.Add(run.SignalHandler(context.Background(), syscall.SIGINT, syscall.SIGTERM))
+	if err := group.Run(); err != nil {
+		utils.Logger.Fatal("Error while running a group", zap.String("err", err.Error()))
 	}
-
 }
--- congratulationsGenerator/go.mod
+++ congratulationsGenerator/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/e1esm/protobuf/bridge_to_API/gen_proto v1.0.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sashabaranov/go-openai v1.8.0
 	go.uber.org/zap v1.24.0
@@ -13,7 +14,16 @@ require (
 replace github.com/e1esm/protobuf/bridge_to_API/gen_proto => ./../protobuf/bridge_to_API/gen_proto
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/oklog/run v1.1.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.8.0 // indirect

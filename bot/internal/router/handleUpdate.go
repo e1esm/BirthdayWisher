cd411236c20ceec5e29fd402bbe53e6580a2bc27
elesm <elesmdev1102@gmail.com>
2023-05-24 21:02:13 +0300

    User's data is being deleted when one's left

--- bot/internal/router/handleUpdate.go
+++ bot/internal/router/handleUpdate.go
@@ -1,6 +1,9 @@
 package router
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"BirthdayWisherBot/utils"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func (r *BirthdayRouter) HandleUpdate(update tgbotapi.Update) {
 
@@ -11,6 +14,13 @@ func (r *BirthdayRouter) HandleUpdate(update tgbotapi.Update) {
 	if update.Message != nil {
 		if update.Message.IsCommand() {
 			r.PickCommand(update)
+		}
+		if user := update.Message.LeftChatMember; user != nil {
+			err := r.ConnectorService.DeleteUser(user.ID, update.FromChat().ID)
+			if err != nil {
+				utils.Logger.Error("Couldn't have delete a left user")
+				return
+			}
 		}
 	}
 }
